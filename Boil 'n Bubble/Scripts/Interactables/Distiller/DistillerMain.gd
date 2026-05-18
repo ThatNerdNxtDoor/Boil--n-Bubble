9971df55a54b2ae6351ba8618d1083799e7e6e26
ThatNerdNxtDoor <thatnerdnxtdoor122@gmail.com>
2026-05-18 17:04:42 -0400

    0.1.547
    + Added Sprite for Distilled Mixture.
    + Added Saving Functionality to the Distiller.
    = Fixed Sprite Ingredients not Appearing in the Distiller once placed.

--- Boil 'n Bubble/Scripts/Interactables/Distiller/DistillerMain.gd
+++ Boil 'n Bubble/Scripts/Interactables/Distiller/DistillerMain.gd
@@ -4,7 +4,7 @@ extends Node3D
 @onready var animation : AnimationPlayer = $AnimationPlayer
 @onready var burner_timer : Timer = $DistillerTimer
 @onready var burner_particles : CPUParticles3D = $Burner/StatusParticles
-@onready var container_sprites : Node = $"RetopodDistiller/DistillerInteractable/Container Sprite"
+@onready var container_sprites : Node = $RetopodDistiller/DistillerInteractable/ContainerSprites
 
 var contained_ingredients = []
 var full_mixture = null
@@ -18,9 +18,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	for child in contained_ingredients.size():
-		container_sprites.get_child(child).texture = load(contained_ingredients[child]["icon"])
-	pass
+	if (contained_ingredients.size() > 0):
+		for child in contained_ingredients.size():
+			(container_sprites.get_child(child)).texture = load(contained_ingredients[child]["icon"])
 
 #Change Color of mixture
 func change_mixture_color(color : Color):
@@ -39,17 +39,16 @@ func activate(function, caller):
 				if (contained_ingredients.size() > 0):
 					if (contained_ingredients[0] == mat_dictionary):
 						contained_ingredients.append(mat_dictionary)
-						print("hmmm")
 				else:
 					contained_ingredients.append(mat_dictionary)
 				PlayerInventory.inventory[PlayerInventory.holding_index] = null
+				print(contained_ingredients)
 			else:
 				pass
 		"hatch": #Open/close the hatch
 			if (!door_lock):
 				open = !open
 				animation.play("OpenHatch") if open else animation.play_backwards("OpenHatch")
-			pass
 		"distill": #Distill the ingredients into a potent and more stable mixture
 			if (open):
 				open = false
@@ -58,7 +57,7 @@ func activate(function, caller):
 				var main_ingredient = contained_ingredients[0];
 				full_mixture = {
 					"name": "mixture",
-					"icon": "res://Assets/Sprites/PotionBottle.png",
+					"icon": "res://Assets/Sprites/MixtureResult.png",
 					"type": "mixture",
 					"color": [(main_ingredient["color"])[0],
 							  (main_ingredient["color"])[1],
@@ -68,9 +67,9 @@ func activate(function, caller):
 					"potency": (main_ingredient["potency"] * contained_ingredients.size()) * .75,
 					"complexity": (main_ingredient["complexity"] * contained_ingredients.size()) * .5,
 				}
+				door_lock = true
 				burner_particles.emitting = true
 				burner_timer.start()
-			pass
 		"collect": #Collect the mixture from the beaker
 			if (mixture_mesh.visible):
 				var index = PlayerInventory.inventory.find(null)
@@ -80,13 +79,34 @@ func activate(function, caller):
 					contained_ingredients = []
 					full_mixture = null
 					mixture_mesh.visible = false
+					door_lock = false
 				else:
 					print('inventory full')
-				pass
 
+#Burner timer ending completes the distilling
 func burner_timeout():
 	burner_particles.emitting = false
 	mixture_mesh.visible = true
 	change_mixture_color(Color(full_mixture["color"][0], full_mixture["color"][1], full_mixture["color"][2]))
-	for child in container_sprites.get_children():
-		child.texture = null
+
+#----------------------------Save Management Functions----------------------------
+func save():
+	var save_dictionary = {
+		"contained_ingredients": contained_ingredients,
+		"full_mixture": full_mixture,
+		"open": open,
+		"burner_timer": burner_timer.time_left,
+	}
+	return save_dictionary
+
+func load_save(load_data):
+	contained_ingredients = load_data["contained_ingredients"]
+	full_mixture = load_data["full_mixture"]
+	open = load_data["open"]
+	if (full_mixture != null): #Mixture is brewing or ready to collect.
+		if (load_data["burner_timer"] > 0.0):
+			burner_timer.start(load_data["burner_timer"])
+			door_lock = true
+		else:
+			burner_timeout()
+	
